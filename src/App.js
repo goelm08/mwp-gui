67bc26f3fe6f5225458e22d56979e35c0a7f4141
Mayank Goel <goelm08@gmail.com>
2022-12-29 16:49:03 +0530

    new and final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import List from "./Components/List";
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputText, setInputText] = useState([]);
+    const [message, setMessage] = useState("");
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    };
+
+    let inputHandler = (e) => {
+        //convert input text to lower case
+        console.log(message)
+        // const lowerCase = message.toLowerCase();
+        const params = {
+            question: message,
+        };
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            json:true,
+            body: JSON.stringify( params )
+        };
+
+        fetch("/solve", options).then((res) =>
+            res.json().then((data) => {
+                debugger;
+                const ans_list = data['list']
+                setInputText(ans_list);
+                console.log(data);
+            })
+        );
+
+    };
+
+    return (
+        <div className="main">
+            <h1>React Search</h1>
+            <div className="search">
+                <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    onChange={handleChange}
+                    fullWidth
+                    label="Search"
+                />
+            </div>
+            <button onClick={inputHandler}>
+                Search
+            </button>
+            <List input={inputText} />
+        </div>
+    );
 }
 
 export default App;
